8a67e05fc70b0ce670ff4dd713c2e92c0ec5fa72
valentina14h <paulahrr14@gmail.com>
2026-05-26 15:57:26 +0000

    cambiar mensaje de bienvenida

--- src/app/app.ts
+++ src/app/app.ts
@@ -43,7 +43,7 @@ export class Server implements ServerApp {
 
     // Ruta raíz de prueba para verificar que el servidor está activo
     this.app.get("/", (_req: Request, res: Response) => {
-      res.send("Hello World");
+      res.send("Hello profe");
     });
 
     // Inicia el servidor HTTP y comienza a escuchar en el puerto configurado
